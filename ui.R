bb3f35dc89b3358369a14d5d8824e00fe53e82d8
avidys <jm@avidys.com>
2014-06-13 16:53:04 -0700

    add gh-pages and slidify

--- ui.R
+++ ui.R
@@ -1,28 +1,40 @@
 library(shiny)
 library(ggplot2)
 
-diamonds <- diamonds[c('carat','color','price')]
-dataset <- diamonds
-
 shinyUI(pageWithSidebar(
-  
-  headerPanel("Diamonds Explorer"),
-  
+  headerPanel("Example plot"),
   sidebarPanel(
-    
-    sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
-                value=min(1000, nrow(dataset)), step=500, round=0),
-    
-    selectInput('x', 'X', names(dataset), 'carat'),
-    selectInput('y', 'Y', names(dataset), 'price'),
-    selectInput('color', 'Color', c('None', names(dataset)), 'color'),
-    
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth', value=T)
-    
+    sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)
   ),
-  
   mainPanel(
-    plotOutput('plot')
+    plotOutput('newHist')
   )
 ))
+
+
+# diamonds <- diamonds[c('carat','color','price')]
+# dataset <- diamonds
+# 
+# shinyUI(pageWithSidebar(
+#   
+#   headerPanel("Diamonds Explorer"),
+#   
+#   sidebarPanel(
+#     
+#     sliderInput('sampleSize', 'Sample Size', min=1, max=nrow(dataset),
+#                 value=min(1000, nrow(dataset)), step=500, round=0),
+#     
+#     selectInput('x', 'X', names(dataset), 'carat'),
+#     selectInput('y', 'Y', names(dataset), 'price'),
+#     selectInput('color', 'Color', c('None', names(dataset)), 'color'),
+#     
+#     checkboxInput('jitter', 'Jitter'),
+#     checkboxInput('smooth', 'Smooth', value=T)
+#     
+#   ),
+#   
+#   mainPanel(
+#     plotOutput('plot')
+#   )
+# ))
+# 
